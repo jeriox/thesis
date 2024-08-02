bbadcf126c47c3618778febcd51baf9c1d73882c
Julian B <jeriox@users.noreply.github.com>
2024-08-02 22:37:02 +0200

    convert yaramo instance to railml and pass to railml2b

--- yaramo-chaincode/verify_yaramo_instance.py
+++ yaramo-chaincode/verify_yaramo_instance.py
@@ -1,8 +1,16 @@
 import argparse
 import json
+from railml_exporter.exporter import Exporter
+from yaramo.topology import Topology
+import subprocess
 
 parser = argparse.ArgumentParser()
 parser.add_argument("yaramo_json")
 
 args = parser.parse_args()
-instance = json.loads(args.yaramo_json)
+topology = Topology.from_json(args.yaramo_json)
+railml_filename = f"data/topology_{topology.uuid}.xml"
+Exporter(topology).to_file(railml_filename)
+result = subprocess.run(["java", "-jar", "railml2b.jar", railml_filename], capture_output=True, encoding="utf-8")
+result.check_returncode()
+assert "failed rules: 0" in result.stdout
